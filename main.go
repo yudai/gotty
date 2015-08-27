6e39085a53f70e37872d5d8914acbd4579ebfb46
Iwasaki Yudai <yudai@arielworks.com>
2015-08-26 23:25:55 -0700

    Release v0.0.4
    * New `--title-format` option to specify window titles
    * Terminal profile support is added
    * ARM architecture support
    * New `--auto-reconnect` option to reconnect to the server
    * TLS/SSL option by `-t` option
    * `--once` option to shutdown the server after the first client exits
    * Better UTF-8 input support

--- main.go
+++ main.go
@@ -13,7 +13,7 @@ import (
 
 func main() {
 	cmd := cli.NewApp()
-	cmd.Version = "0.0.3"
+	cmd.Version = "0.0.4"
 	cmd.Name = "gotty"
 	cmd.Usage = "Share your terminal as a web application"
 	cmd.HideHelp = true
