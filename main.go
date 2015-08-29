f133c28cf869c4c45c9eba2b849728196a718845
Iwasaki Yudai <yudai@arielworks.com>
2015-08-30 07:04:48 +0900

    Release v0.0.6
    * Profile file is merged into config file

--- main.go
+++ main.go
@@ -13,7 +13,7 @@ import (
 
 func main() {
 	cmd := cli.NewApp()
-	cmd.Version = "0.0.5"
+	cmd.Version = "0.0.6"
 	cmd.Name = "gotty"
 	cmd.Usage = "Share your terminal as a web application"
 	cmd.HideHelp = true
