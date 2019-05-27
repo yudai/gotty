ad5e904928d5eacffffd4cb662bbb3c0b502d9e5
navigaid <navigaid@gmail.com>
2019-05-28 02:01:54 +0800

    client: basic display functionality

--- client/client.go
+++ client/client.go
@@ -1,6 +1,16 @@
 package client
 
-import "net"
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+	"net"
+	"os"
+
+	"github.com/kr/pty"
+
+	"github.com/yudai/gotty/wetty"
+)
 
 type Client struct {
 	addr string
@@ -19,6 +29,49 @@ func (c *Client) Run() error {
 	if err != nil {
 		return err
 	}
+
+	// read/write conn
 	_ = conn
+
+	// get terminal size
+	size, err := pty.GetsizeFull(os.Stdin)
+	if err != nil {
+		return err
+	}
+	type termSize struct {
+		Columns uint16
+		Rows    uint16
+	}
+	resize, err := json.Marshal(termSize{size.Cols, size.Rows})
+	if err != nil {
+		return err
+	}
+
+	// os.Stderr.Write(resize)
+
+	conn.Write(append([]byte{wetty.ResizeTerminal}, resize...))
+	// defer exec.Command("reset").Run()
+
+	buf := make([]byte, 1024)
+	for {
+		n, err := conn.Read(buf)
+		if err != nil {
+			if err == io.EOF {
+				return nil
+			}
+			return err
+		}
+		if n == 0 {
+			println("continue")
+			continue
+		}
+		switch buf[0] {
+		case wetty.Output:
+			os.Stdout.Write(buf[1:n])
+		default:
+			io.WriteString(os.Stderr, fmt.Sprintf("unrecognized message type: `%b`", buf[0]))
+		}
+	}
+
 	return nil
 }
